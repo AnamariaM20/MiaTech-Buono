1429e3fd21b79f2dfcdb4fadeb023146550088b7
AnamariaM20 <anamaria.munteanu2024@gmail.com>
2024-08-21 09:53:35 +0300

    added first 10 ex

---
+++ javascript/index.js
@@ -0,0 +1,89 @@
+//es 1
+let numero = 1;
+console.log(numero);
+
+const age = 30; 
+console.log(age);
+
+//es2 
+
+let streetNumber = 5;
+streetNumber = 10;
+console.log(streetNumber);
+
+//es3
+
+let number = 3 
+{
+    let number = 5
+    console.log(number)
+}
+console.log(number)
+
+//es4
+let outside = 12;
+{
+    let inside = 20
+    console.log(outside);
+    console.log(inside);
+}
+    console.log(outside);
+    console.log(inside);
+
+//es5
+//cosa non va?
+/*
+let i = 2; 
+for (; i <= 20; i++) {
+    return i % 2 == 0
+    console.log(i)
+    invece di "i" se puo utilizaare il nome de la variabile?
+    se io voglio fare una variabile con i numeri pari?
+    cosa vuol dire  --- let result = "";
+}
+*/ 
+for (let i = 2; i <= 20; i++) {
+    return i % 2 == 0
+}
+console.log(i)
+
+//6
+let num = 10
+while (num = 10) {
+    return i % 2 == 1;
+    num--
+  }
+
+//7
+
+let word = prompt("impecable")
+//8
+console.log(word.length)
+
+let word1 = [3, 5, 6, 10]
+console.log(word1.length)
+
+let word2 = "Maria"
+console.log(word2.length)
+
+//9
+let world = "impecabile"
+do (i = 0; i < world.length; i++) {
+    console.log(myArray[i]);
+    
+} while (i == 5) {
+    Break;}
+
+
+//10
+
+let books = 100;
+if(books >= 5){
+    console.log("la variabila e maggiore di 5")
+} else {
+    console.log("la variabila e minore di 5")
+}
+
+
+
+    
