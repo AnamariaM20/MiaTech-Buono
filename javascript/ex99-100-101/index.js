b18ef70dfe0c3bc04641e1658aca62fab5edba5b
AnamariaM20 <anamaria.munteanu2024@gmail.com>
2024-10-07 17:39:14 +0300

    added 99,100,101

---
+++ javascript/ex99-100-101/index.js
@@ -0,0 +1,8 @@
+//99
+
+let paragraph = document.getElementsByTagName("p")
+console.log(paragraph)
+//100
+const elementId = document.getElementById("#1")
+//101
+let $classe = document.querySelector(".para")
