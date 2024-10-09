915745c0bd5247b04c2c4a165f56b307cbb28318
AnamariaM20 <anamaria.munteanu2024@gmail.com>
2024-10-10 01:02:02 +0300

    added 8 es

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors:{
+      'blumarine': '#6DC0D5',
+      "pulsante1" : "#B2CEDE",
+      "pulsante2" : ""
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      color: {
+        "primary": "#AD7A99",
+      },
+      spacing: {
+      },
+      margin: {
+        miomargin: "27px"
+      },
+      padding: {
+        miopadding: "7px"
+      }
+    },
+  },
+  plugins: [],
+}
